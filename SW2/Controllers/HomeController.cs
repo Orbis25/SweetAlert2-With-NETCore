f6547a95268695920b11a4c81fa7e0f916d24ad3
orbis25 <orbisalonzo@gmail.com>
2020-10-20 20:53:02 -0400

    Add project files.

---
+++ SW2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SW2.Extensions;
+using SW2.Models;
+
+namespace SW2.Controllers
+{
+    public class HomeController : BaseController
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            //el metodo de crear retorno true
+            if (true)
+            {
+                BasicNotification("Creado", NotificationType.Success, "Correcto!");
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
